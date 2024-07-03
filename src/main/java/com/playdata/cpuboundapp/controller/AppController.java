093047df9840648354fd38895b134e36cde3c46d
Cherish <49014223+lcl1380@users.noreply.github.com>
2024-07-03 16:54:51 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "<h1 style='color:red;'>변경 중 . . .</h1>";
+        return "<h1 style='color:red;'>변경 완료!</h1>";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
